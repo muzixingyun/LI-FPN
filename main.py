9bd287f0372d75f99673de103e4da7fb76364f75
Xingyun Li <41226100+muzixingyun@users.noreply.github.com>
2023-08-09 22:30:41 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import argparse
-from Model import LI_FPN, DC, LIM, STFPN
+
+from torch import nn, optim
+
+from Model import LI_FPN
+from Util import train_test_util_binary
 import torch
 
 parser = argparse.ArgumentParser('LI_FPN_Arg')
@@ -10,6 +14,12 @@ parser.add_argument('--lim', type=str, default='g', help='{g for LI_G, s for LI_
 parser.add_argument('--backbone', type=str, default='res18', help='{res18, res34, res50}')
 parser.add_argument('--len_t', type=int, default=7, help='the length of input')
 parser.add_argument('--pretrain', type=bool, default=False)
+parser.add_argument('--batch_size', type=int, default=4)
+parser.add_argument('--lr', type=float, default=1e-4)
+parser.add_argument('--epoch', type=int, default=100)
+parser.add_argument('--model_id', type=str, default='define your model name')
+parser.add_argument('--log_path', type=str, default='log path, a txt file')
+parser.add_argument('--model_save_path', type=str, default='where the trained model is saved, a folder path')
 args = parser.parse_args()
 print(args)
 
@@ -21,9 +31,19 @@ if __name__ == '__main__':
                           backbone=args.backbone,
                           len_t=args.len_t,
                           pretrain=args.pretrain).to(device)
-    # print(model)
-    a = torch.rand((4, 7, 3, 224, 224)).to(device)
-    b, c, d = model(a)
-    print(b.shape)
-    print(c.shape)
-    print(d.shape)
+    train_data_set = 'the train dataset'
+    test_data_set = 'the test dataset'
+    Loss = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=args.lr)
+    train_test_util_binary.train_class(epoch_num=args.epoch,
+                                       train_data_set=train_data_set,
+                                       test_data_set=test_data_set,
+                                       batch_size=args.batch_size,
+                                       net=model,
+                                       Loss=Loss,
+                                       optimizer=optimizer,
+                                       is_use_gpu=True,
+                                       model_id=args.model_id,
+                                       eval_num=1,
+                                       log_path=args.log_path,
+                                       model_save_path=args.model_save_path)
